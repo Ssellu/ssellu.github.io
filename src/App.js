e3945b32940c2ec159b2938b5628cf3373c296cd
Ssellu <ssellu.lee@gmail.com>
2023-01-31 23:05:09 +0900

    Add Routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import "./assets/js/util.js";
 import { ThemeProvider } from "./features/theme";
 import React, { useState, useEffect } from "react";
 import { HelmetProvider } from "react-helmet-async";
+import { Route, Routes, useLocation } from "react-router-dom";
+
 import BodyComponent from "./main";
 
 function App() {
+  const location = useLocation();
+  console.log("pathname", location.pathname);
+
   const [themeName, setThemeName] = useState("main");
   useEffect(() => {
     // Your theme name determining logic
@@ -15,7 +20,9 @@ function App() {
   return (
     <HelmetProvider>
       <ThemeProvider value={themeName}>
-        <BodyComponent />
+        <Routes>
+          <Route exact path="/" element={<BodyComponent />} />
+        </Routes>
       </ThemeProvider>
     </HelmetProvider>
   );
